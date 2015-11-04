d4972a16437836f4fcc628a0690ce14acecfdba6
abeytaadam <measalion@gmail.com>
2015-11-03 19:04:39 -0800

    Spotify lists

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 // wait for DOM to load before running JS
-$(function() {
+$(document).ready(function () {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
   
-  // your code here
+	$('#search').on('submit', function (event){
+		event.preventDefault();
+		
+		var keyword = $('#tracks').val();
+		
+		$.get('https://api.spotify.com/v1/search?q='+ keyword +'&type=track', function (data) {
+ 			var trackName = data.tracks.items;
+ 			console.log(trackName);
+ 			trackName.forEach(function (track){
+ 				$('#songList').append('<div id= artSong>' + 'Track: ' + track.name + '<br /> ' + 'Artist: ' + track.artists[0].name + '<br /><hr><br />' + '</div>');
+ 				$('#aCover').append('<a href="'+ track.preview_url + '">' + '<img src="' + track.album.images[0].url + '">' + '</a>' + '<br /><br />');
+ 			});
+
+ 		});
+		$('#songList').text('');
+		$('#aCover').text('');
+	});
+
 
 });
+
+
+
+
